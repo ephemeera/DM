22872bf728bd04400a78c850966070a64f43b304
ephemeera <79689444+ephemeera@users.noreply.github.com>
2021-04-16 10:28:33 +0400

    Add files via upload

--- lab2.js
+++ lab2.js
@@ -2,10 +2,24 @@ function antrefl(arr) {
 	size = arr.length;
 	for (int i = 0; i < size; i++) {
 		if (arr[i][i] != 0) {
-			 document.getElementById("antrefl").innerHTML = "нет";  
+			document.getElementById("antrefl").innerHTML = "нет";  
 			return false;
 		}
 	}
         document.getElementById("antrefl").innerHTML = "да";   
+	return true;
+}
+
+function antsim(arr) {
+	size = arr.length;
+	for (int i = 0; i < size; i++) {
+		for (int j = 0; j < size; j++) {
+			if (arr[i][j] == 1 && arr[j][i] == 1) {
+				document.getElementById("antsim").innerHTML = "нет";  
+				return false;
+			}
+		}
+	}
+	document.getElementById("antsim").innerHTML = "да";   
 	return true;
 }
