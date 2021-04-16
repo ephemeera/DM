f2e5620e36442604a25a01552d71fcded3a1e5ac
ephemeera <79689444+ephemeera@users.noreply.github.com>
2021-04-16 10:50:05 +0400

    Add files via upload

--- lab2.js
+++ lab2.js
@@ -40,13 +40,26 @@ function сossim(arr) {
 
 function refl(arr) {
 	size = arr.length;
-
 	for (int i = 0; i < size; i++) {
 		if (arr[i][i] != 1) {
 			document.getElementById("refl").innerHTML = "нет";
 			return false;
 		}
 	}
-	document.getElementById("refl").innerHTML = "нет";
+	document.getElementById("refl").innerHTML = "да";
+	return true;
+}
+
+function sim(arr) {
+	size = arr.length;
+	for (int i = 0; i < size; i++) {
+		for (int j = 0; j < size; j++) {
+			if (arr[i][j] != arr[j][i]) {
+				document.getElementById("sim").innerHTML = "нет";
+				return false;
+			}
+		}
+	}
+	document.getElementById("sim").innerHTML = "да";
 	return true;
 }
