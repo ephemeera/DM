35ad2f1c45867b63bbfb81d857ef93b083a0f9cc
ephemeera <79689444+ephemeera@users.noreply.github.com>
2021-04-23 20:12:48 +0400

    Add files via upload

--- lab2.js
+++ lab2.js
@@ -1,118 +1,99 @@
 function antrefl(arr) {
 	size = arr.length;
-	for (int i = 0; i < size; i++) {
+	for (var i = 0; i < size; i++) {
 		if (arr[i][i] != 0) {
-			document.getElementById("antrefl").innerHTML = "нет";  
-			return false;
+			return "нет";
 		}
 	}
-        document.getElementById("antrefl").innerHTML = "да";   
-	return true;
+	return "да";
 }
 
-function antsim(arr) {
+function cossim(arr) {
 	size = arr.length;
-	for (int i = 0; i < size; i++) {
-		for (int j = 0; j < size; j++) {
-			if (arr[i][j] == 1 && arr[j][i] == 1) {
-				document.getElementById("antsim").innerHTML = "нет";  
-				return false;
+	for (var i = 0; i < size; i++) {
+		for (var j = 0; j < size; j++) {
+			if (arr[i][j] == arr[j][i] && i != j) {
+				return "нет";
 			}
 		}
 	}
-	document.getElementById("antsim").innerHTML = "да";   
-	return true;
-}
-
-function сossim(arr) {
-	size = arr.length;
-	for (int i = 0; i < size; i++) {
-		for (int j = 0; j < size; j++) {
-			if (arr[i][j] != -arr[j][i]) {
-				document.getElementById("сossim").innerHTML = "нет";
-				return false;
-			}
-		}
-	}
-	document.getElementById("сossim").innerHTML = "да"; 
-	return true;
+	return "да";
 }
 
 function refl(arr) {
 	size = arr.length;
-	for (int i = 0; i < size; i++) {
+	for (var i = 0; i < size; i++) {
 		if (arr[i][i] != 1) {
-			document.getElementById("refl").innerHTML = "нет";
-			return false;
+			return "нет";
 		}
 	}
-	document.getElementById("refl").innerHTML = "да";
-	return true;
+	return "да";
 }
 
 function sim(arr) {
 	size = arr.length;
-	for (int i = 0; i < size; i++) {
-		for (int j = 0; j < size; j++) {
+	for (var i = 0; i < size; i++) {
+		for (var j = 0; j < size; j++) {
 			if (arr[i][j] != arr[j][i]) {
-				document.getElementById("sim").innerHTML = "нет";
-				return false;
+				return "нет";
 			}
 		}
 	}
-	document.getElementById("sim").innerHTML = "да";
-	return true;
+	return "да";
 }
 
 function trans(arr) {
 	size = arr.length;
-	var arr2 = [];
-	for (int i = 0; i < size; i++) {
-		var arr2 = [];
-		for (int j = 0; j < size; j++) {
+	var arr2 = new Array(size);
+	for (var i = 0; i < size; i++) {
+		arr2[i] = new Array(size);
+		for (var j = 0; j < size; j++) {
 			arr2[i][j] = 0;
-			for (int g = 0; g < size; g++) {
+			for (var g = 0; g < size; g++) {
 				arr2[i][j] += arr[i][g] * arr[g][j];
 			}
 		}
 	}
-	for (int i = 0; i < size; i++) {
-		for (int j = 0; j < size; j++) {
+	for (var i = 0; i < size; i++) {
+		for (var j = 0; j < size; j++) {
 			if (arr2[i][j] != 0 && arr[i][j] == 0) {
-				document.getElementById("trans").innerHTML = "нет";
-				return false;
+				return "нет";
 			}
 		}
 	}
-	document.getElementById("trans").innerHTML = "да";
-	return true;
+	return "да";
 }
 
 function table() {
-	int input = document.getElementById("input").value.split(/\s/);
-	size = document.getElementById("size").value;
-	var arr = [];
-	for (int i = 0; i < size; i++) {
-		arr[i] = [];
-		for (int j = 0; j < size; j++) {
+	var input = document.getElementById("input").value.split("\n");
+	r = document.getElementById("r").value;
+	var arr = new Array(r);
+	for (var i = 0; i < r; i++) {
+		arr[i] = new Array(r);
+		for (var j = 0; j < r; j++) {
 			arr[i][j] = 0;
 		}
 	}
-	if (input.length % 2 == 0) {
-		for (int i = 0; i < input.length; i += 2) {
-			arr[input[i + 1] - 1][input[i] - 1] = 1;
+	for (var i = 0; i < input.length; i++) {
+		input[i] = input[i].split(" ");
+		for (var j = 0; j < input.length; j += 2) {
+			arr[input[i][0] - 1][input[i][1] - 1] = 1;
 		}
 	}
-	else {
-		confirm("Лишние координаты!");
-	}
+		
 	var output = new String();
-	for (int i = 0; i < size; i++) {
-		for (int j = 0; j < size; j++) {
+	for (var i = 0; i < r; i++) {
+		for (var j = 0; j < r; j++) {
 			output += arr[i][j];
 			output += " ";
 		}
 		output += "\n";
 	}
+		
 	document.getElementById("output").value = output;
+	document.getElementById("antrefl").innerHTML = antrefl(arr);
+	document.getElementById("cossim").innerHTML = cossim(arr);
+	document.getElementById("refl").innerHTML = refl(arr);
+	document.getElementById("sim").innerHTML = sim(arr);
+	document.getElementById("trans").innerHTML = trans(arr);
 }
