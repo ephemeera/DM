6f6cc661bafd4ee40529f5ed636a17d28c548869
ephemeera <79689444+ephemeera@users.noreply.github.com>
2021-04-23 22:34:52 +0400

    Add files via upload

--- lab3.js
+++ lab3.js
@@ -15,3 +15,31 @@ function table() {
     document.getElementById('table').innerHTML = matr; 
 
 }
+
+function check() {
+	var n = document.getElementById("n").value;
+	var mas = document.getElementsByClassName("cell");
+	var g = 0;
+	var count = 0;
+	for (var i = 0; i < n; i++) {
+		var k = 0;
+		for(var j = 0; j < n; j++) {
+			if(mas[g].value == 1) {
+				k++;
+			}
+			g++;
+		}
+	if(k == 1) {
+		count++;
+	}
+	}
+	if(count == n) {
+		document.getElementById('check').innerHTML = 'Отношение является функцией';
+	}else{
+		document.getElementById('check').innerHTML = 'Отношение не является функцией';
+	}
+}
+
+function run() {
+	check();
+}
